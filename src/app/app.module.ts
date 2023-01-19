d172d86dcd6967d1345f83c2bc9d852fc78cd8a5
JennyLamego2022 <jennylamego@hotmail.com>
2023-01-19 19:00:47 -0300

    feat: criação de novo componente card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './componentes/form/form.component';
+import { CardComponent } from './componentes/card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormComponent
+    FormComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
